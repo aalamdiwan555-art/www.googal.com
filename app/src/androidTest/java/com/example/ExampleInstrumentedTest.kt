5c3beb76d6929a14d7de8b9743c85a7cf2ba2f82
aalamdiwan555-art <aalamdiwan555@gmail.com>
2026-06-29 20:21:20 +0530

    Update ExampleInstrumentedTest.kt

--- app/src/androidTest/java/com/example/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/ExampleInstrumentedTest.kt
@@ -1,22 +1,21 @@
 package com.example
 
-import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.platform.app.InstrumentationRegistry
-import org.junit.Assert.*
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
-@RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+@RunWith(RobolectricTestRunner::class)
+@Config(sdk = [34])
+class ExampleRobolectricTest {
+
   @Test
-  fun useAppContext() {
-    // Context of the app under test.
-    val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-    assertEquals("com.example", appContext.packageName)
+  fun `read string from context`() {
+    val context = ApplicationProvider.getApplicationContext<Context>()
+    val appName = context.getString(R.string.app_name)
+    assertEquals("AutoClicker Pro", appName)
   }
 }
